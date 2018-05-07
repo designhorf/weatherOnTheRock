18dfce2c94dc37285bb6ef80db1f6c1052293702
Ferenc Horvath <ferhor@MT-STUDIO-0001.local>
2018-05-07 10:08:34 +0200

    created ThreeHoursForecast component

---
+++ src/components/ThreeHoursForecast/ThreeHoursForecast.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import HoursForecast from './../HoursForecast';
+import { getThreeHoursForecast } from '../../api';
+
+
+export default class ThreeHoursForecast extends Component {
+    constructor (props) {
+        super(props);
+    
+        this.state = {
+            threeHoursForecast: []
+        };
+    }
+
+    componentDidMount () {
+        getThreeHoursForecast('Sunday')
+            .then(threeHoursForecast => this.setState({ threeHoursForecast }));
+    }
+
+    render () {
+        const { threeHoursForecast } = this.state;
+
+        if (threeHoursForecast.length === 0) {
+            return (
+                <div className="loading">Loading...</div>
+            );
+        }
+
+        console.log(threeHoursForecast);
+
+        return (
+            <section className="three-hours-forecast">
+                {
+                    threeHoursForecast.map(day =>
+                        <HoursForecast 
+                            icon={ day.icon } 
+                            temperature={ day.mainTemperature } 
+                            hour = { day.hour }
+                            windSpeed = { day.windSpeed }
+                            humidity = { day.humidity }
+                        />
+                    )
+                }
+            </section>    
+        );
+    }
+}
