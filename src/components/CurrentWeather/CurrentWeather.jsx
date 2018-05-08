e61e2023f9ac3b33d3ac03f95faaeab3dde4d35a
Ferenc Horvath <ferhor@MT-STUDIO-0001.local>
2018-05-08 13:30:39 +0200

    deleted unnecessary commented code snippets

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -21,8 +21,6 @@ export default class CurrentWeather extends Component {
     render () {
         const { currentWeather } = this.state;
 
-        // console.log(currentWeather);
-
         if (currentWeather.length === 0) {
             return (
                 <div>Loading...</div>
@@ -35,14 +33,14 @@ export default class CurrentWeather extends Component {
                 <p className="date">{ currentWeather.currentDate }</p>
 
                 <section className="main-info">
-                     <div className="icon">
-                         <img className="img-icon" src={`../../assets/images/icons/${ currentWeather.icon }.svg`} alt=""/>
-                         <p className="description">{ currentWeather.icon }</p>
-                     </div>
-                     <div className="temperature">{ currentWeather.temperature }°</div>
-                 </section>
+                    <div className="icon">
+                        <img className="img-icon" src={`../../assets/images/icons/${ currentWeather.icon }.svg`} alt=""/>
+                        <p className="description">{ currentWeather.icon }</p>
+                    </div>
+                    <div className="temperature">{ currentWeather.temperature }°</div>
+                </section>
 
-                 <section className="widget-extra">
+                <section className="widget-extra">
                     <CurrentDetails 
                         type = 'humidity'
                         icon = 'Humidity'
@@ -64,49 +62,8 @@ export default class CurrentWeather extends Component {
                         format = 'km/h'
                         value = { currentWeather.windSpeed.toFixed(0) }
                     />
-
-                    
-                    {/* <div className="widget-extra-box wind-speed">
-                        <img src="./assets/images/icons/Rainy.svg" alt="" className="widget-extra-icon wind-speed-icon"/>
-                        <p className="wind-speed-value"></p>
-                    </div> */}
-                 </section>
+                </section>
             </div>    
         );
     }
 }
-
-
-// export default function CurrentWeather ({ city, date }) {
-//     return (
-//         <div>
-//             <h1 className="city">{ city }</h1>
-//             <p className="date">{ date }</p>
-
-//             <section className="main-info">
-//                 <div className="icon">
-//                     <img className="img-icon" src="" alt=""/>
-//                     <p className="description"></p>
-//                 </div>
-//                 <div className="temperature"></div>
-//             </section>
-
-//             <section className="widget-extra">
-//                 <div className="widget-extra-box humidity">
-//                     <img src="./assets/images/icons/Humidity.svg" alt="" className="widget-extra-icon humidity-icon"/>
-//                     <p className="humidity-value"></p>
-//                 </div>
-
-//                 <div className="widget-extra-box wind-direction">
-//                     <img src="./assets/images/icons/Wind-Direction.svg" alt="" className="widget-extra-icon wind-direction-icon"/>
-//                     <p className="wind-direction-value"></p>
-//                 </div>
-
-//                 <div className="widget-extra-box wind-speed">
-//                     <img src="./assets/images/icons/Rainy.svg" alt="" className="widget-extra-icon wind-speed-icon"/>
-//                     <p className="wind-speed-value"></p>
-//                 </div>
-//             </section>
-//         </div>
-//     )
-// }
